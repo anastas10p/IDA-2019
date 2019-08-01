097dd70f75d7c0ff979a5f361730dd6a4789d5cc
Anastasios <a.tsigkros@gmail.com>
2019-08-01 21:41:52 +0200

    Split id into id, producer, factory and keep those columns.

--- Case study/tidy_dataset.R
+++ Case study/tidy_dataset.R
@@ -7,11 +7,11 @@ tidy_dataset <- function(component_data){
   for (id_column in colnames(select(component_data, starts_with("ID")))){
     component_data[[id_column]] <- as.character(component_data[[id_column]])
   }
-  #Herstellernummer
+  #Herstellernummer possibly useless
   for (producer_column in colnames(select(component_data, starts_with("Herstellernummer")))){
     component_data[[producer_column]] <- as.integer(component_data[[producer_column]])
   }
-  #Werksnummer
+  #Werksnummer possibly useless
   for (factory_column in colnames(select(component_data, starts_with("Werksnummer")))){
     component_data[[factory_column]] <- as.integer(component_data[[factory_column]])
   }
@@ -19,7 +19,7 @@ tidy_dataset <- function(component_data){
   for (faulty_column in colnames(select(component_data, starts_with("Fehlerhaft")))){
     component_data[[faulty_column]] <- as.logical(component_data[[faulty_column]])
   }
-  #Datum
+  #Produktionsatum
   #If the date is set as an origin date:
   if ("origin" %in% colnames(component_data)){
     for (production_date_column in colnames(select(component_data, starts_with("Produktionsdatum")))){
@@ -43,13 +43,13 @@ tidy_dataset <- function(component_data){
                          try(component_data[[colnames(select(component_data, starts_with("ID")))[2]]]),
                          try(component_data[[colnames(select(component_data, starts_with("ID")))[3]]])))
   component_data$id <- coalesce(!!!id_cols)
-  
+  #to be deleted
   producer_cols <- Filter(Negate(function(x) is.null(unlist(x))), 
                           list(try(component_data$Herstellernummer), 
                                try(component_data$Herstellernummer.x), 
                                try(component_data$Herstellernummer.y)))
   component_data$producer_number <- coalesce(!!!producer_cols)
-  
+  #to be deleted
   factory_cols <- Filter(Negate(function(x) is.null(unlist(x))), 
                          list(try(component_data$Werksnummer), 
                               try(component_data$Werksnummer.x), 
@@ -68,40 +68,13 @@ tidy_dataset <- function(component_data){
                              try(component_data$Fehlerhaft.y)))
   component_data$faulty <- coalesce(!!!faulty_cols)
   
-  
-  #Select the new columns and delete the old data to save memory.
+  #Select the new columns and delete the old data to save memory. Change this accordingly.
   component_data_clean <- select(component_data, "id", "producer_number", "factory_number", "faulty", "production_date")
   rm(component_data)
   
   # #Check the clean data for mistakes by comparing the data in the id with the corresponding columns.
-  # split_ids <- str_split(string = component_data_clean$id, pattern = "-")
-  # 
-  #To be subbed with something without a loop.
-  for(entry_nr in c(1:nrow(component_data_clean))){
-    
-    id_split <- str_split(test_table[entry_nr, ])
-    id_producer <- id_split[2]
-    id_factory <- id_split[3]
-    
-    #Check producer
-    if(component_data_clean$producer[entry_nr] != id_producer){
-      #Print message and correct
-      print(paste("Correcting component's", component_data_clean$id[entry_nr],
-                  "producer number from", component_data_clean$producer[entry_nr],
-                  "to the id information", id_producer))
-      component_data_clean$producer[entry_nr] <- id_producer
-    }
-    
-    #Check factory
-    if(component_data_clean$dactroy[entry_nr] != id_factory){
-      #Print message and correct
-      print(paste("Correcting component's", component_data_clean$id[entry_nr],
-                  "factory number from", component_data_clean$factory[entry_nr],
-                  "to the id information", id_factory))
-      component_data_clean$factory[entry_nr] <- id_factory
-    }
-    
-    #We no longer need the full product ID, so we only keep the component code.
-    component_data$id[entry_nr] <- id_split[1]
-  }
+  component_data_clean <- component_data_clean %>%
+    separate(id, c("id", "producer", "factory", "number"), "-") %>%
+    select("id", "producer", "factory",  "faulty", "production_date")
+
 }
