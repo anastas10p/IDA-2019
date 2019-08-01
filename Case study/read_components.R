8644af16e66dfd9965ae445af3ae5169153a3de7
Marina Matthaiou <m.matthaiou@campus.tu-berlin.de>
2019-08-01 23:03:00 +0200

    Update read_components.R

--- Case study/read_components.R
+++ Case study/read_components.R
@@ -3,15 +3,40 @@
 #read data using read.delim and the separators 
 #K1
   
-K1be1 <-  read.delim(paste("Data/Komponente/Komponente_K1BE1.csv",sep = ""), head = TRUE,",")
-K1be2 <-  read.delim(paste("Data/Komponente/Komponente_K1BE2.csv",sep = ""), head = TRUE, ";")
-K1di1 <-  read.delim(paste("Data/Komponente/Komponente_K1DI1.csv",sep = ""), head = TRUE, ",")
+K1be1 <-  read.delim(paste("Data/Komponente/Komponente_K1BE1.csv",sep = ""), head = TRUE, sep = ",")
+K1be2 <-  read.delim(paste("Data/Komponente/Komponente_K1BE2.csv",sep = ""), head = TRUE, sep = ";")
+K1di1 <-  read.delim(paste("Data/Komponente/Komponente_K1DI1.csv",sep = ""), head = TRUE, sep = ",")
+#K1di2 
+fileName1 <- 'Data/Komponente/Komponente_K1DI2.txt'
+striga <- readChar(fileName1, file.info(fileName1)$size)
+striga <- gsub("\\", "|", striga, fixed = TRUE)
+striga <- gsub("\"", "", striga, fixed = FALSE)
+striga <- gsub("\t", "", striga, fixed = FALSE)
+read.delim(striga,  header = TRUE, sep = "|")
+write_lines( striga,"striga_reduced.txt")
+# es gibt manche whitespaces im txt file 
+K1di2 <- read.delim(paste("striga_reduced.txt", sep = "") , header = TRUE , sep ="|")
+
   
   
 #K2
-K2le2 <-  read.delim(paste("Data/Komponente/Komponente_K2LE2.txt",sep = ""), header = TRUE, "\\")
+K2le2 <-  read.delim(paste("Data/Komponente/Komponente_K2LE2.txt",sep = ""), header = TRUE, sep = "\\")
 K2st1 <-  read.delim(paste("Data/Komponente/Komponente_K2ST1.txt",sep = ""), header = TRUE, sep = "|")
 K2st2 <-  read.delim(paste("Data/Komponente/Komponente_K2ST2.csv",sep = ""), header = TRUE, sep = ";")
+#K2le1
+fileName2 <- 'Data/Komponente/Komponente_K2LE1.txt'
+strigaros <- readChar(fileName2, file.info(fileName2)$size)
+strigaros <- gsub("II", "|", strigaros, fixed = TRUE)
+strigaros <- gsub("\"", "|", strigaros, fixed = TRUE)
+strigaros <- gsub("\x0B", "", strigaros, fixed = FALSE)
+#strigaros <- gsub(strigaros, fixed = FALSE))
+strigaros <- gsub(" ", "", strigaros, fixed = FALSE)
+strigaros <- gsub("|||", "|", strigaros, fixed = TRUE)
+strigaros <- gsub("||", "|", strigaros, fixed = TRUE)
+read.delim(strigaros,  header = TRUE, sep = " ")
+write_lines( strigaros ,"strigaros_reduced.txt")
+# es gibt manche whitespaces im txt file 
+K2le1 <- K1di2 <- read.delim(paste("strigaros_reduced.txt", sep = "") , header = TRUE , sep =" ")
 
 #K3
 
