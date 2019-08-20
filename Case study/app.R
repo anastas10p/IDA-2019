d77996dfbbf7d2d41329f1cfbdef4f9354fc8279
Daniel <daniel.schewitz@outlook.de>
2019-08-20 19:54:11 +0200

    3b fertig, Paretos angepasst, Producer hinzugefügt

--- Case study/app.R
+++ Case study/app.R
@@ -8,10 +8,6 @@ if(!require(ggplot2)){
 }
 library(ggplot2)
 
-if(!require(plotly)){
-  install.packages("plotly")
-}
-library(plotly)
 
 if(!require(cowplot)){
   install.packages("cowplot")
@@ -23,51 +19,9 @@ if(!require(dplyr)){
 }
 library(dplyr)
 
-load(paste(getwd(),"/dataset_3a.RData",sep=""))
 
-#define function for plotting the pareto diagram
-pareto <- function(df, f, e){
-     
-    #filter factory
-    df <- filter(df, factory == f)
-      
-    if (e == "rel"){
-        df$error_data <- df$rel_error
-        ymax <- 0.3
-        ylabel <- "Relative Error"
-    }
-    if (e == "abs"){
-        df$error_data <- df$abs_error
-        ymax <- 25000
-        ylabel <- "Absolute Error"
-    }
-    
-    if (length(df$factory != 0)){
-        #descending order
-        df <- df[order(df$error_data, decreasing=TRUE), ]
-        
-        #make factors for x-axis
-        df$id <- factor(df$id, levels= unique(df$id))  
-        
-        
-        #new column cumulative relative error
-        df$cum_error <- cumsum(df$error_data)
-        
-        #generate the Pareto chart
-        ggplot(df, aes(x=df$id, y=df$error_data)) +
-            geom_bar(aes(fill=df$id),stat = "identity", width = length(df$id)*0.2) +
-            scale_y_continuous(limits = c(0,ymax), sec.axis = sec_axis(~./max(df$cum_error/2), name = "Cumulated")) +
-            geom_point(aes(y=df$cum_error/2), size=2) +
-            geom_path(aes(y=df$cum_error/2, group=1), size=0.5) +
-            labs(title = df$factory, x = "Components", y = ylabel) +
-            theme(legend.position="none") +
-            scale_fill_manual(values = c("K1BE1"=colors()[10], "K1BE2"=colors()[20], 
-                              "K1DI1"=colors()[50], "K2LE2"=colors()[100], "K2ST1"=colors()[30],
-                              "K2ST2"=colors()[80], "K3AG1"=colors()[70], "K3AG2"=colors()[60], "K3SG1"=colors()[90],
-                              "K3SG2"=colors()[40], "K4"=colors()[400], "K5"=colors()[450],
-                              "K6"=colors()[500], "K7"=colors()[550]))
-        }
-}
+load(paste(getwd(),"/dataset_app.RData",sep=""))
+
 
 ############### UI ###############
 ui <- fluidPage(
@@ -79,7 +33,6 @@ ui <- fluidPage(
     
     tabsetPanel(type = "tabs",
                 tabPanel("Info",
-                         style = "overflow-y:scroll; max-height: 500px",
                          img(src="car.jpeg", height="70%", width="70%", align="center"),
                          p(strong("Description")),
                          p({"The present app is designed to cope with large amount of data in the automotive sector.
@@ -104,16 +57,14 @@ ui <- fluidPage(
                 
                 tabPanel("Pareto Plots", 
                          #Scrollable tab
-                         style = "overflow-y:scroll; max-height: 500px",
-                         
-                         
+                        
                          fluidRow(
                            column(
                              width = 6,
                              h4("Selection of the year"),
                              #dropdown for production year
                              selectInput("year", "Production Year:", 
-                                         choices = 2008:2016)
+                                         choices = errors_by_id$production_date, selected = TRUE)
                            ),
                            column(
                              width = 6,
@@ -142,7 +93,7 @@ ui <- fluidPage(
                          ),
                          
                          
-                         plotOutput("Pareto2")),
+                         plotOutput("Pareto2", height = "600px")),
                          
                 tabPanel("Error on component-sharp resolution",
                          
@@ -152,13 +103,13 @@ ui <- fluidPage(
                              h4("Selection of the year"),
                              #dropdown for production year
                              selectInput("year_line", "Production Year:", 
-                                         choices = 2008:2016)
+                                         choices = errors_by_id$production_date, selected = TRUE)
                            ),
                            column(
                              width = 6,
                              h4("Selection of the component"),
                              #switch between rel/abs error
-                             selectInput("component", "Select component for detailed error plot", choices = errors_by_id$id)
+                             selectInput("component", "Select component for detailed error plot", choices = names(errors_by_id_week))
                            )
                          ),
                          
@@ -176,34 +127,10 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     
     #select input to filter by year for pareto diagram
-    year_input <- reactive({input$year
-        switch(input$year,
-               "2008" = 2008,
-               "2009" = 2009,
-               "2010" = 2010,
-               "2011" = 2011,
-               "2012" = 2012,
-               "2013" = 2013,
-               "2014" = 2014,
-               "2015" = 2015,
-               "2016" = 2016)
-    })
-    
+    year_input <- reactive({input$year})
     
     #Input for line diagram
-    year_line_input <- reactive({input$year_line
-      switch(input$year,
-             "2008" = 2008,
-             "2009" = 2009,
-             "2010" = 2010,
-             "2011" = 2011,
-             "2012" = 2012,
-             "2013" = 2013,
-             "2014" = 2014,
-             "2015" = 2015,
-             "2016" = 2016)
-      })
-    
+    year_line_input <- reactive({input$year_line})
     
     error_input <- reactive({
         switch(input$error,
@@ -212,24 +139,24 @@ server <- function(input, output, session) {
     })
     
     
-    component_input <- reactive({
-      input$component
-      })
+    component_input <- reactive({input$component})
    
   
     #Pareto diagram to compare factories
     output$Pareto1 <- renderPlot({
-        y <- year_line_input()
+        y <- year_input()
         errors_by_factory <- filter(errors_by_factory, production_date == y)
         e <- error_input()
         if (e == "rel"){
           errors_by_factory$error_data <- errors_by_factory$rel_error
           ymax <- 0.25
+          sec_axis_factor <- 9
           ylabel <- "Relative Error"
         }
         if (e == "abs"){
           errors_by_factory$error_data <- errors_by_factory$abs_error
           ymax <- 40000
+          sec_axis_factor <- 6
           ylabel <- "Absolute Error"
         }
         errors_by_factory <- errors_by_factory[order(errors_by_factory$error_data, decreasing=TRUE), ]
@@ -237,9 +164,10 @@ server <- function(input, output, session) {
         errors_by_factory$cum_error <- cumsum(errors_by_factory$error_data)
         ggplot(errors_by_factory, aes(x=errors_by_factory$factory, y=errors_by_factory$error_data)) +
             geom_bar(aes(fill = errors_by_factory$factory), stat = "identity")  +
-            scale_y_continuous(limits = c(0,ymax), sec.axis = sec_axis(~./max(errors_by_factory$cum_error/9), name = "Cumulated")) +
-            geom_point(aes(y=errors_by_factory$cum_error/9), size=2) +
-            geom_path(aes(y=errors_by_factory$cum_error/9, group=1)) +
+            scale_y_continuous(limits = c(0,ymax), sec.axis = sec_axis(~./max(errors_by_factory$cum_error/sec_axis_factor), name = "Cumulated")) +
+            geom_point(aes(y=errors_by_factory$cum_error/sec_axis_factor), size=2) +
+            geom_path(aes(y=errors_by_factory$cum_error/sec_axis_factor, group=1)) +
+            geom_hline(yintercept=max(errors_by_factory$cum_error/sec_axis_factor), linetype = "dashed") +
             labs(title = "Errors by Factory", x = "Factories", y = ylabel) +
             theme(legend.position="none")
     })
@@ -258,6 +186,50 @@ server <- function(input, output, session) {
     
     #Pareto diagrams to compare components
     output$Pareto2 <- renderPlot({
+      #define function for plotting the pareto diagram
+      pareto <- function(df, f, e){
+        
+        #filter factory
+        df <- filter(df, factory == f)
+        
+        if (e == "rel"){
+          df$error_data <- df$rel_error
+          ymax <- 0.3
+          ylabel <- "Relative Error"
+        }
+        if (e == "abs"){
+          df$error_data <- df$abs_error
+          ymax <- 25000
+          ylabel <- "Absolute Error"
+        }
+        
+        if (length(df$factory != 0)){
+          #descending order
+          df <- df[order(df$error_data, decreasing=TRUE), ]
+          
+          #make factors for x-axis
+          df$id <- factor(df$id, levels= unique(df$id))  
+          
+          
+          #new column cumulative relative error
+          df$cum_error <- cumsum(df$error_data)
+          
+          #generate the Pareto chart
+          ggplot(df, aes(x=df$id, y=df$error_data)) +
+            geom_bar(aes(fill=df$id),stat = "identity", width = length(df$id)*0.2) +
+            scale_y_continuous(limits = c(0,ymax), sec.axis = sec_axis(~./max(df$cum_error/2), name = "Cumulated")) +
+            geom_point(aes(y=df$cum_error/2), size=2) +
+            geom_path(aes(y=df$cum_error/2, group=1), size=0.5) +
+            labs(title = df$factory, x = "Components", y = ylabel) +
+            theme(legend.position="none", aspect.ratio=1) +
+            scale_fill_manual(values = c("K1BE1"=colors()[10], "K1BE2"=colors()[20], 
+                                         "K1DI1"=colors()[50], "K2LE2"=colors()[100], "K2ST1"=colors()[30],
+                                         "K2ST2"=colors()[80], "K3AG1"=colors()[70], "K3AG2"=colors()[60], "K3SG1"=colors()[90],
+                                         "K3SG2"=colors()[40], "K4"=colors()[400], "K5"=colors()[450],
+                                         "K6"=colors()[500], "K7"=colors()[550]))
+        }
+      }
+      
         y <- year_input()
         errors_by_id <- filter(errors_by_id, production_date == y)
         e <- error_input()
@@ -281,17 +253,21 @@ server <- function(input, output, session) {
     output$Linediagram <- renderPlot({
       validate(
         need(input$component, "Please select a component"))
-      selected_component <- input$component
-      total_error_data <- errors_by_id %>%
-        group_by(id, production_date) %>% 
+      selected_component <- component_input()
+      selected_year <- year_line_input()
+      total_error_data <- errors_by_id_week[[selected_component]] %>%
+        filter(production_year == selected_year) %>%
+        group_by(production_week) %>%
         summarise(total_error = sum(abs_error))
-      df <- errors_by_id %>%
-        left_join(total_error_data, by = c("id","production_date")) %>%
-        filter(id == selected_component)
-      ggplot(df, aes(x = df$production_date, group = df$factory)) +
-        geom_line(aes(y = df$abs_error, color = df$factory)) +
-        geom_line(aes(y = df$total_error, color = "total")) +
-        labs(x = "Production Date", y = "Absolute Error", color = "Factory")
+      df <- data.frame(errors_by_id_week[[selected_component]]) %>%
+        filter(production_year == selected_year) %>%
+        left_join(data.frame(total_error_data), by = c("production_week"))
+      df$factory <- factor(df$factory)
+      ggplot(df, aes(x = df$production_week, group = df$factory)) +
+        geom_line(aes(y = df$abs_error, color = df$factory),size = 1) +
+        geom_point(aes(y = df$abs_error, color = df$factory), size = 2) +
+        geom_line(aes(y = df$total_error, color = "total"),size = 1.3, linetype = "dotted") +
+        labs(x = "Production Week", y = "Absolute Error", color = "Factory")
     })
       
     
@@ -300,7 +276,7 @@ server <- function(input, output, session) {
 #but ordering the factors for the x-axis happens only once
     
     #Show the basic dataset
-     output$dataset <- renderDataTable(rename(errors_by_id, component = id), options = list(pageLength = 10))
+    output$dataset <- renderDataTable(rename(errors_by_id, component = id), options = list(pageLength = 10))
 }
 
 # Run the application 
