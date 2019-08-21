c97a047ea6b57d3a7c116e47158f6cc52d5f04fb
Anastasios <anastasios.tsigkros@ipk-projekt.fraunhofer.de>
2019-08-21 03:02:33 +0200

    Alle Dateien werden richtig abgelesen.

--- Case study/read_components.R
+++ Case study/read_components.R
@@ -7,15 +7,11 @@ k1be1 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K1BE1.csv",s
 k1be2 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K1BE2.csv",sep = ""), head = TRUE, ";"))#ok
 k1di1 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K1DI1.csv",sep = ""), head = TRUE, ","))#ok
 #K1di2 
-# fileName1 <- 'Data/Komponente/Komponente_K1DI2.txt'
-# striga <- readChar(fileName1, file.info(fileName1)$size)
-# striga <- gsub("\\", "|", striga, fixed = TRUE)
-# striga <- gsub("\"", "", striga, fixed = FALSE)
-# striga <- gsub("\t", "", striga, fixed = FALSE)
-# read.delim(striga,  header = TRUE, sep = "|")
-# write_lines( striga,"striga_reduced.txt")
-# # es gibt manche whitespaces im txt file 
-# k1di2 <- read.delim(paste("striga_reduced.txt", sep = "") , header = TRUE , "|")
+k1di2 <- readChar('Data/Komponente/Komponente_K1DI2.txt', file.info('Data/Komponente/Komponente_K1DI2.txt')$size)
+k1di2 <- gsub('[[:blank:]]', '\\', k1di2)
+k1di2 <- gsub('""', '"\n"', k1di2)
+
+k1di2 <- tidy_dataset(read.delim(text = k1di2, header = TRUE , sep = '\\'))
 
   
   
@@ -24,24 +20,16 @@ k2le2 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K2LE2.txt",s
 k2st1 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K2ST1.txt",sep = ""), header = TRUE, "|"))#ok
 k2st2 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K2ST2.csv",sep = ""), header = TRUE, ";"))#ok
 #K2le1
-# fileName2 <- 'Data/Komponente/Komponente_K2LE1.txt'
-# strigaros <- readChar(fileName2, file.info(fileName2)$size)
-# strigaros <- gsub("II", "|", strigaros, fixed = TRUE)
-# strigaros <- gsub("\"", "|", strigaros, fixed = TRUE)
-# strigaros <- gsub("\x0B", "", strigaros, fixed = FALSE)
-# #strigaros <- gsub(strigaros, fixed = FALSE))
-# strigaros <- gsub(" ", "", strigaros, fixed = FALSE)
-# strigaros <- gsub("|||", "|", strigaros, fixed = TRUE)
-# strigaros <- gsub("||", "|", strigaros, fixed = TRUE)
-# read.delim(strigaros,  header = TRUE, sep = " ")
-# write_lines( strigaros ,"strigaros_reduced.txt")
-# # es gibt manche whitespaces im txt file 
-# K2le1 <- read.delim(paste("strigaros_reduced.txt", sep = "") , header = TRUE , " ")
+
+K2le1 <- readChar('Data/Komponente/Komponente_K2LE1.txt', file.info('Data/Komponente/Komponente_K2LE1.txt')$size)
+K2le1 <- gsub('\x0B"', '\n"', K2le1)
+K2le1 <- gsub('II', ' ', K2le1)
+K2le1 <- tidy_dataset(read.delim(text = K2le1, header = TRUE , sep = ' '))
 
 #K3
 
 k3ag1 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K3AG1.csv",sep = ""), header = TRUE, ","))#ok
-k3ag2 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K3AG2.txt", sep = ""), header = TRUE, "\\"))#ok
+k3ag2 <- read.delim(paste("Data/Komponente/Komponente_K3AG2.txt", sep = ""), header = TRUE, "\\")#ok
 k3sg1 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K3SG1.csv",sep = ""), header = TRUE, ","))#ok
 k3sg2 <-  tidy_dataset(read.delim(paste("Data/Komponente/Komponente_K3SG2.csv",sep = ""), header = TRUE, ","))#ok
 
